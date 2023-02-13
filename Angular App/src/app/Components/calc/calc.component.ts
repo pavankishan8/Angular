7fc41df5e03c89ccb1d1f6eeca92c8454d60b5c5
Pavan R <101443408+pavankishan8@users.noreply.github.com>
2023-02-13 14:58:23 +0530

    Add files via upload

---
+++ Angular App/src/app/Components/calc/calc.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent {
+    value1 : number = 0.0;
+    value2 : number = 0.0;
+    operand : string = "";
+    result : number = 0.0;
+
+    onSubmit(){
+      switch(this.operand){
+        case "+":
+          this.result = this.value1 + this.value2;
+          break;
+          case "-":
+          this.result = this.value1 - this.value2;
+          break;
+          case "*":
+          this.result = this.value1 * this.value2;
+          break;
+          case "/":
+          this.result = this.value1 / this.value2;
+          break;
+      }
+    }
+}
--- Angular App/src/app/app.module.ts
+++ Angular App/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { ContentComponent } from './Components/content/content.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { CalcComponent } from './Components/calc/calc.component';
+import { MasterComponent } from './Components/master/master.component';
 
 @NgModule({
   declarations: [
@@ -12,10 +14,13 @@ import { FooterComponent } from './Components/footer/footer.component';
     HeaderComponent,
     ContentComponent,
     FooterComponent,
+    CalcComponent,
+    MasterComponent,
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
